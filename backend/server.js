04735b2aaa342c7a03ec1600c8a4457a8f5da5c9
AyonDasDhrubo <129670008+AyonDasDhrubo@users.noreply.github.com>
2025-09-05 12:51:31 +0600

    Update server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -32,6 +32,12 @@ mongoose
 app.get("/", (_req, res) => res.send("Server running!"));
 
 // Start
-app.listen(5000, () =>
-  console.log("Server running on http://localhost:5000")
+// app.listen(5000, () =>
+//   console.log("Server running on http://localhost:5000")
+// );
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
 );
+
